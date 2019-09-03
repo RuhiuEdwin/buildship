b973c675ccd7d5f89aa81822c19ec33a5e01b4df
Donat Csikos <csdonat@gmail.com>
2019-09-03 17:06:16 +0200

    Add missing dependencies to full test coverage

--- .teamcity/Buildship/Check30/Checkpoints/buildTypes/FullTestCoverage.kt
+++ .teamcity/Buildship/Check30/Checkpoints/buildTypes/FullTestCoverage.kt
@@ -36,9 +36,13 @@ object FullTestCoverage : BuildType({
         snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse44, CheckpointUtils.DefaultFailureCondition)
         snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse45, CheckpointUtils.DefaultFailureCondition)
         snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse46, CheckpointUtils.DefaultFailureCondition)
+        snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse47, CheckpointUtils.DefaultFailureCondition)
         snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse47Java9, CheckpointUtils.DefaultFailureCondition)
         snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse48, CheckpointUtils.DefaultFailureCondition)
+        snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse49, CheckpointUtils.DefaultFailureCondition)
         snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse410, CheckpointUtils.DefaultFailureCondition)
+        snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse411, CheckpointUtils.DefaultFailureCondition)
+        snapshot(Buildship.Check30.FullTestCoverage.Linux.buildTypes.Eclipse413, CheckpointUtils.DefaultFailureCondition)
         snapshot(Buildship.Check30.FullTestCoverage.Windows.buildTypes.Eclipse44, CheckpointUtils.DefaultFailureCondition)
         snapshot(Buildship.Check30.FullTestCoverage.Windows.buildTypes.Eclipse47, CheckpointUtils.DefaultFailureCondition)
     }
