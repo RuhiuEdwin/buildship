0c841e91462cfacd0bcfaf14e72b139a8a2a2a26
Donat Csikos <csdonat@gmail.com>
2019-10-09 13:55:39 +0200

    Add sanity check build

---
+++ .teamcity/Buildship/Check30/SanityCheck/buildTypes/Eclipse413.kt
@@ -0,0 +1,23 @@
+package Buildship.Check30.SanityCheck.buildTypes
+
+import Buildship.EclipseBuildTemplate
+import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
+
+object Eclipse413 : BuildType({
+    id("Sanity_Check_Linux_Eclipse413_Java8_30")
+    name = "Basic Test Coverage (Linux, Eclipse 2019-09, Java 8)"
+
+    templates(EclipseBuildTemplate)
+
+    params {
+        param("eclipse.version", "413")
+        param("compiler.location", "%linux.java8.oracle.64bit%/bin/javac")
+        param("eclipse.test.java.home", "%linux.java8.oracle.64bit%")
+        param("gradle.tasks", "assemble")
+        param("env.JAVA_HOME", "%linux.java8.oracle.64bit%")
+    }
+
+    requirements {
+        contains("teamcity.agent.jvm.os.name", "Linux", "RQ_650")
+    }
+})
